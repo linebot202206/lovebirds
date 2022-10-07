3db22423078346f4b30e6aba444ca903f5e50bfb
linebot202206 <107710883+linebot202206@users.noreply.github.com>
2022-10-08 00:43:09 +0800

    Update toDo.php

--- includes/toDo.php
+++ includes/toDo.php
@@ -1,15 +1,153 @@
 <?php
 global $client, $message, $event;
-//require_once('./connection.php');
+require_once('connection.php');
 
-$client->replyMessage(array(
-    'replyToken' => $event['replyToken'],
-    'messages' => array(
-        array(
-        'type' => 'text', //訊息類型 (文字)
-        'text' => "第一個文字"
-        )
-    )
-));
+$name = "必做清單";
+$sql = "SELECT * FROM `toDoList` WHERE `name` = '".$name."'";
+$toDo = mysqli_query( $conn, $sql );
+if($toDo) {
+	$list = mysqli_fetch_array($toDo, MYSQLI_ASSOC);
+
+	if($list){
+		$id = $list['id'];
+		$sql = "SELECT * FROM `toDoList_detail` WHERE `id` = ".$id;
+		$detail = mysqli_query( $conn, $sql );
+		$data = [];
+		while ($res = mysqli_fetch_array($detail, MYSQLI_ASSOC)) {
+			$data[] = $res;
+		}
+		if($data){
+			$icon = ["❎","✅"];
+			$contents = [];
+			foreach ($data as $value) {
+				$contents[] = [
+					'type' => "box",
+					'layout' => "horizontal",
+					'contents' => [
+						[
+							'type' => "text",
+							'text' => $icon[$value['finish']],
+							'size' => "sm",
+							'color' => "#555555",
+							'flex' => 1,
+						],
+						[
+							'type' => "text",
+							'text' =>  $value['name'],
+							'size' => "sm",
+							'color' => "#111111",
+							'flex' => 8,
+						]
+					],
+				];
+			}
+
+			$title = [
+				[
+					'type' => "text",
+					'text' => "全部",
+					'weight' => "bold",
+					'color' => "#1DB446",
+					'size' => "sm",
+				],
+				[
+					'type' => "text",
+					'text' => $list['name'],
+					'weight' => "bold",
+					'size' => "xxl",
+					'margin' => "md",
+				],
+			];
+
+			$separator = [
+				[
+					'type' => "separator",
+					'margin' => "xxl",
+				],
+			];
+
+			$time = [
+				[
+					'type' => "box",
+					'layout' => "horizontal",
+					'margin' => "md",
+					'contents' => [
+						[
+							'type' => "text",
+							'text' => "建立時間",
+							'size' => "xs",
+							'color' => "#aaaaaa",
+							'flex' => 0,
+						],
+						[
+							'type' => "text",
+							'text' => date('Y/m/d H:i', $list['creationTime']),
+							'size' => "xs",
+							'color' => "#aaaaaa",
+							'align' => "end",
+						],
+					],
+				],
+				[
+					'type' => "box",
+					'layout' => "horizontal",
+					'margin' => "md",
+					'contents' => [
+						[
+							'type' => "text",
+							'text' => "更新時間",
+							'size' => "xs",
+							'color' => "#aaaaaa",
+							'flex' => 0,
+						],
+						[
+							'type' => "text",
+							'text' => date('Y/m/d H:i', $list['updatenTime']),
+							'size' => "xs",
+							'color' => "#aaaaaa",
+							'align' => "end",
+						],
+					],
+				],
+			];
+
+			$box = [
+				[
+					'type' => "box",
+					'layout' => "vertical",
+					'margin' => "xxl",
+					'spacing' => "sm",
+					'contents' => $contents,
+				],
+			];
+
+			$out = [
+				'type' => "bubble",
+				'body' => [
+					'type' => "box",
+					'layout' => "vertical",
+					'contents' => array_merge($title, $separator, $box, $separator, $time),
+				],
+				'style' => [
+					'footer' => [
+						'separator' => true,
+					],
+				],
+			];
+            
+            $client->replyMessage(array(
+                'replyToken' => $event['replyToken'],
+                'messages' => array(
+                    array(
+                        'type' => 'flex', //訊息類型 (flex)
+                        'altText' => '清單', //替代文字
+                        'contents' => $out //Flex Message 內容
+                    )
+                )
+            ));
+			
+		}
+	}
+}
 
 ?>
