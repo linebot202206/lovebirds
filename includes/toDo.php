566d899d5dd0281243d41f375bafbbf8b059d473
linebot202206 <107710883+linebot202206@users.noreply.github.com>
2022-10-08 00:40:07 +0800

    Update toDo.php

--- includes/toDo.php
+++ includes/toDo.php
@@ -2,6 +2,14 @@
 global $client, $message, $event;
 //require_once('./connection.php');
 
-
+$client->replyMessage(array(
+    'replyToken' => $event['replyToken'],
+    'messages' => array(
+        array(
+        'type' => 'text', //訊息類型 (文字)
+        'text' => "第一個文字"
+        )
+    )
+));
 
 ?>
