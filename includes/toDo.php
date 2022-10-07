aa412b5a897d08754854e93e4660ce29c1d4a848
linebot202206 <107710883+linebot202206@users.noreply.github.com>
2022-10-08 01:20:42 +0800

    Update toDo.php

--- includes/toDo.php
+++ includes/toDo.php
@@ -46,7 +46,6 @@ if($toDo) {
 			}
 			*/
 
-			/*
 			$title = [
 				[
 					'type' => "text",
@@ -63,7 +62,6 @@ if($toDo) {
 					'margin' => "md",
 				],
 			];
-			*/
 
 			/*
 			$separator = [
