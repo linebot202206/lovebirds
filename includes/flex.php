abe3b4536f4781be17bf73de26ddff422796a235
linebot202206 <107710883+linebot202206@users.noreply.github.com>
2022-10-08 00:27:11 +0800

    Update flex.php

--- includes/flex.php
+++ includes/flex.php
@@ -4,17 +4,7 @@ global $client, $message, $event;
 $type = explode(" ",$message['text'])[0];
 
 if($type == "#清單"){
-    //require_once('./toDo.php');
-
-    $client->replyMessage(array(
-        'replyToken' => $event['replyToken'],
-        'messages' => array(
-            array(
-                'type' => 'text', //訊息類型 (文字)
-                'text' => "第一個文字"
-            )
-        )
-    ));
+    require_once('./toDo.php');
 }
 
 ?>
