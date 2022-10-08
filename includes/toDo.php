e80286aa22ca15194ece0045968c47f230836ad6
linebot202206 <107710883+linebot202206@users.noreply.github.com>
2022-10-08 17:16:16 +0800

    Update toDo.php

--- includes/toDo.php
+++ includes/toDo.php
@@ -146,16 +146,17 @@ if($toDo) {
 				],
 			];
 			
-        $client->replyMessage(array(
-            'replyToken' => $event['replyToken'],
-            'messages' => array(
-                array(
-                    'type' => 'flex', //訊息類型 (flex)
-                    'altText' => 'Example flex message template', //替代文字
-                    'contents' => $out //Flex Message 內容
-                )
-            )
-        ));
+
+	    $client->replyMessage(array(
+	        'replyToken' => $event['replyToken'],
+	        'messages' => array(
+	            array(
+	                'type' => 'text', //訊息類型 (文字)
+	                'text' => $list['name']
+	                //'text' => 'Hello, world!'.$profile['displayName'] //回覆訊息
+	            )
+	        )
+	    ));
 			
 			
 		}
